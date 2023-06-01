74ce5c92468c65854c0cea69c5bd31578e0748c6
laviniafranqueiro <133999323+laviniafranqueiro@users.noreply.github.com>
2023-06-01 12:48:58 -0300

    Create tarefa2.py

---
+++ tarefa2.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd 
+st.title('tarefa do Josir')
+st.caption('Lavínia Franqueiro')
+
+df = pd.read_csv('carprice.csv', sep =',')
+st.dataframe(df)
+
+chart_data = df[['make', 'wheel-base', 'height']]
+st.bar_chart(chart_data)
