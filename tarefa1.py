aff9cb43ff71cf71c87c34aeab3962ba000380a2
laviniafranqueiro <133999323+laviniafranqueiro@users.noreply.github.com>
2023-06-01 12:38:37 -0300

    Create tarefa1.py

---
+++ tarefa1.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd 
+st.title('tarefa 1 do Josir')
+st.caption('Lavínia Franqueiro')
+
+df = pd.read_csv('carprice.csv', sep =';')
+st.dataframe(df)
+
+#arquivo = open('carprice.csv')
+#for linha in arquivo:
+#    st.write(linha)
